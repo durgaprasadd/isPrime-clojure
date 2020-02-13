512f44965ec992459e05e45918a6fd6368eca52c
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2020-02-13 13:13:57 +0530

    converted map future with pmap

--- src/is_prime_clojure/core.clj
+++ src/is_prime_clojure/core.clj
@@ -2,17 +2,16 @@
 
 (defn my-is-prime?
   [number end total-parts start]
-  (some zero?
-        (map mod (repeat number)
-             (range start end total-parts))))
+  (not (some zero?
+             (map mod (repeat number)
+                  (range start end total-parts)))))
 
 
 (defn -main [& args]
-  (let [number 67280421310721 cores 8 end (inc (Math/sqrt number))
-        (time
-          (println
-            (not
-              (some deref
-                    (doall (map
-                             #(future (my-is-prime? number end cores %))
-                             (range 2 (inc (inc cores)))))))))]))
+  (let [number 67280421310721 cores 8 end (inc (Math/sqrt number))]
+    (time
+      (println
+        (every? identity
+                (doall (pmap
+                         (partial my-is-prime? number end cores)
+                         (range 2 (inc (inc cores))))))))))
